74668300ae2b6172f71e3fef9e7b84a28386c3bf
tai21042004 <tai21042004@gmail.com>
2025-11-21 21:56:03 +0700

    refactor(JpaContentUnitRepository): remove unused imports to clean up code and improve readability

--- src/content/src/main/java/co3017/microservices/content_service/repository/postgresql/JpaContentUnitRepository.java
+++ src/content/src/main/java/co3017/microservices/content_service/repository/postgresql/JpaContentUnitRepository.java
@@ -7,10 +7,7 @@ import co3017.microservices.content_service.repository.postgresql.entity.Content
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
